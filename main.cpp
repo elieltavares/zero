2a776d59c894fcfa15ed6250a7d16c1f41830477
elieltavares <elinkpark@gmail.com>
2016-12-08 01:02:58 -0200

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#inclundo <iostream>
+
+using namespace std;
+
+bool life = true;
+bool death = false;
+
+
+int main(int argc,char* argv[]){
+
+  while(life)   //start the Zero Life
+  {
+  
+  
+  
+    if(death)
+    {
+      life = false;
+    }
+  }
+
+
+  return 0;
+}
