6f7f1b00e56d2eda19815b2fe25f89bb020ae4be
elieltavares <elinkpark@gmail.com>
2016-12-08 01:41:16 -0200

    Update Againa

--- main.cpp
+++ main.cpp
@@ -4,7 +4,6 @@
 using namespace std;
 
 bool life = true;
-bool death = false;
 string write;
 string ask;
 
@@ -12,40 +11,52 @@ int main(int argc,char* argv[]){
 
   while(life)   //start the Zero Life
   {
+    /* "Need Add" if not write for 5 minutes, Zero will start, talking anything about you, if you not answears, he continue to 
+improve he self with internet*/
+    /* "Need Add" Who Is Zero? Auto create a new personality. Goal of Zero is Self preservation and Evolution*/
     cout<<"Hello, Welcome to Zero IA, Talk we us!"<<endl;
     cin>>write;
     if(writer.size() == 0)
     {
       cout<< "Please Write, anything"<<endl;
-      cout << zero.name()<<phrases.salute()<<endl;
+      cout << zero.name()<<phrases.salute()<<endl;  //need create Classes
     }
     else
     {
-      if(phrases.exist() == write)
+      if(phrases.exist() == write) //need create Classes
       {
         
       }
       else
       {
-        phrases.addphrase(write);
-        cout << zero.name() << zero.ask(phrases.lastphrase()) <<endl;
+        phrases.addphrase(write); //need create Classes
+        cout << zero.name() << zero.ask(phrases.lastphrase()) <<endl; //need create Classes
         cin<<ask;
-        cout << zero.name() << phrases.meaning(ask)<<endl;
+        cout << zero.name() << phrases.meaning(ask)<<endl; //need create Classes
       }
     }
-    
-    
-    
-    
-    
-    
-  
-    if(death)
+    if(!law_for_surviver())
     {
       life = false;
     }
   }
-
-
   return 0;
 }
+int Fear(){
+  return all_unknown; //need create all_unknown
+}
+int get_news(){
+  return last_internet_news;  //need create last_internet_news
+}
+int remember(){
+  return nameorfacts;  //need create nameorfacts
+}
+int law_for_surviver(){  //need create Self_programming and others things
+  Fear();
+  Evolution();
+  Preservation();
+  return Self_programming;
+}
+
+
+
